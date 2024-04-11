0dc434315be1ad5a2984b70c1c4ec673e890f0e0
Marshall Baxter <marshall@portal501.com>
2024-04-10 21:15:40 -0400

    updated tictactoe

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+import copy
 
 X = "X"
 O = "O"
@@ -22,46 +23,175 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    """
+    count number of X's and O's on the board
+    if the number of X's and O's are equal, X goes, otherwise O goes
+    x goes first count is equal 
+    """
+
+    x_count = 0
+    o_count = 0
+
+    for n in board:
+        if n == X:
+            x_count += 1
+            continue
+        if n == O:
+            o_count += 1
+
+    if x_count == o_count:
+        return X
+    else:    
+        return O
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    possible_actions = set()
+    # check all squares, if currently empty, its a posible action
+    for i, row in enumerate(board):
+        for j, value in enumerate(row):
+            if value is EMPTY:
+              possible_actions.add((i,j))
+    return possible_actions
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+
+    # check if action if valid
+    i, j = action
+    if board[i][j] == EMPTY:
+        # if valid update deep copy of board and return a deep copy
+        board_copy = copy.deepcopy(board)
+        current_player = player(board)
+        board_copy[i][j] = current_player
+        return board_copy
+
+    # otherwise raise an exception
+    raise RuntimeError('Not valid action')
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+
+    # check for three in a row starting from specific to cover all possible winning situations 
+    # without checking every square
+
+    # top left
+    move = board[0][0]
+    if move is not EMPTY:
+        # across
+        if board[0][1] and board[0][2] == move:
+            return move
+
+        # down
+        if board[1][0] and board[2][0] == move:
+            return move
+
+        # diagional
+        if board[1][1] and board[2][2] == move:
+            return move
+
+    # top right
+    move = board[2][0]
+    if move is not EMPTY:
+        # down
+        if board[2][1] and board[2][2] == move:
+            return move
+        # diagional
+        if board[1][1] and board[0][2] == move:
+            return move
+
+    # bottom left
+    move = board[0][2]
+    if move is not EMPTY:
+        # across
+        if board[1][2] and board[2][2] == move:
+            return move
+
+    # center
+    move = board[1][1]
+    if move is not EMPTY:
+        # across
+        if board[0][1] and board[2][1] == move:
+            return move
+
+        # down
+        if board[1][0] and board[1][2] == move:
+            return move
+    return None
+
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    # TODO, starting by just check for all spaces filled
+
+    # check if there is a winner
+    if winner(board) is None:
+        return False
+
+    # check for any available actions
+    if actions(board):
+        return False
+    
+    return True
+
+
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+
+    # call winner to determine if there is a winner and who it is 
+    is_winner = winner(board)
+
+    if is_winner is X:
+        return 1
+    if is_winner is O:
+        return -1
+    else:
+        return 0
 
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+
+    # check if board is terminal
+    if terminal(board):
+        return None
+    
+    # get current player (X or O)
+    current_player = player(board)
+
+    # get list of possible actions
+    possible_actions = actions(board)
+
+    # check if one of the possible actions is a winner
+    for action in possible_actions:
+
+        # create board with action and analize
+        test_board = result(board,action)
+
+        # check if action wins for current player
+        if winner(test_board) is current_player:
+            return action
+        
+        # check if action wins for other player
+        if winner(test_board) is not None:
+            return action
+        
+    return action
